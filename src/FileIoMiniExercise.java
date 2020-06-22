3e1102c2dd30a963e70a779f8ea84d90215e12ee
Laura Davis <laura.l.davis.210@gmail.com>
2020-06-22 14:07:16 -0500

    remove unused imports

--- src/FileIoMiniExercise.java
+++ src/FileIoMiniExercise.java
@@ -1,14 +1,11 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class FileIoMiniExercise {
     public static void main(String[] args) {
